b7dea0d304f686019e90c13f4713ad04936b018c
CJ-Lee01 <chunjie01@live.com>
2025-09-15 15:22:13 +0800

    Add more auth tests

--- client/src/context/Auth.test.js
+++ client/src/context/Auth.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import axios from 'axios';
 import '@testing-library/jest-dom/extend-expect';
 import { useAuth, AuthProvider } from './auth';
@@ -7,6 +7,11 @@ import { beforeEach, describe } from 'node:test';
 
 // Mocking axios.post
 jest.mock('axios');
+
+const ValueCapture = ({captureFn}) => {
+  captureFn();
+  return <></>
+}
 
 describe("AuthProvider Component", () => {
   beforeEach(() => {
@@ -26,15 +31,38 @@ describe("AuthProvider Component", () => {
 
   it("provides default auth info", () => {
     let captureValue;
+    const fn = () => {
+      [captureValue] = useAuth();
+    }
     render(
     <AuthProvider> 
-      { (() => {
-        captureValue = useAuth();
-        return <></>
-      })() }
+      <ValueCapture  captureFn={fn}/>
     </AuthProvider>
     );
     expect(captureValue.user).toBe(null);
     expect(captureValue.token).toBe("");
   });
-})
+  it("loads auth from localStorage and sets axios header", async () => {
+    let captureValue;
+    const fn = () => {
+      [captureValue] = useAuth();
+    };
+
+    const storedAuth = { user: { name: "Bobby" }, token: "123abc" };
+    localStorage.setItem("auth", JSON.stringify(storedAuth));
+
+    render(
+      <AuthProvider>
+        <ValueCapture captureFn={fn} />
+      </AuthProvider>
+    );
+
+    await waitFor(() => {
+      const auth = captureValue;
+      expect(auth.user).toEqual(storedAuth.user);
+      expect(auth.token).toBe(storedAuth.token);
+      expect(axios.defaults.headers.common["Authorization"]).toBe("123abc");
+    });
+  });
+  // TODO: Change of Auth
+});
