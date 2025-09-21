75abadd5a30070c6e8d966e5a2c0f4887923c28a
CJ-Lee01 <chunjie01@live.com>
2025-09-21 18:09:52 +0800

    Add more directories to test

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -23,13 +23,14 @@ export default {
   testMatch: [
     "<rootDir>/client/src/pages/Auth/*.test.js",
     "<rootDir>/client/src/context/*.test.js",
-    "<rootDir>/client/src/components/Routes/*.test.js",
+    "<rootDir>/client/src/components/**/*.test.js",
+    "<rootDir>/client/src/pages/user/*.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
-    "client/src/pages/Auth/**",
+    "client/src/pages/**",
     "client/src/context/**",
     "client/src/components/**",
   ],
