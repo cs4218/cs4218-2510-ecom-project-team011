1fa0726174a6406be9af6fa4f10aba5cd124616e
Arushi Verma <arushiverma050@gmail.com>
2025-10-05 12:05:44 +0800

    fixed authController import

--- controllers/authController.test.js
+++ controllers/authController.test.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { render, screen, act } from '@testing-library/react';
-import { SearchProvider, useSearch } from './search';
 import '@testing-library/jest-dom/extend-expect';
 // Mocks 
 
