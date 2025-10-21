620a0ebd8b6ac99b3e5076ae41540cea4f2e7509
CJ-Lee01 <chunjie01@live.com>
2025-10-22 04:35:13 +0800

    Add integration tests to coverage

--- jest.integration.frontend.config.js
+++ jest.integration.frontend.config.js
@@ -1,39 +1,43 @@
 export default {
   // name displayed during tests
   displayName: "integration (frontend)",
-
+  
   // simulates browser environment in jest
   // e.g., using document.querySelector in your tests
   testEnvironment: "jest-environment-jsdom",
-
+  
   // jest does not recognise jsx files by default, so we use babel to transform any jsx files
   transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
-
+  
   // tells jest how to handle css/scss imports in your tests
   moduleNameMapper: {
     "\\.(css|scss)$": "identity-obj-proxy",
   },
-
+  
   // ignore all node_modules except styleMock (needed for css imports)
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
-
+  
   // only run these tests
   testMatch: [
     "<rootDir>/client/src/pages/**/*.integration.test.js",
     "<rootDir>/client/src/context/*.integration.test.js",
     "<rootDir>/client/src/components/**/*.integration.test.js",
   ],
-
+  
   // jest code coverage
-  collectCoverage: false,
-  // collectCoverageFrom: [
-  //   "client/src/pages/**",
-  //   "client/src/pages/Auth/**",
-  //   "client/src/context/**",
-  //   "client/src/components/**",
-  // ],
+  collectCoverage: true,
+  
+  collectCoverageFrom: [
+    "client/src/pages/**",
+    "client/src/pages/Auth/**",
+    "client/src/context/**",
+    "client/src/components/**",
+  ],
+  
+  coverageDirectory: "coverage/ifrontend",
+  
   // coverageThreshold: {
   //   global: {
   //     lines: 100,
